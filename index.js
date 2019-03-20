36934b497a0576a27c16f55d04857c99259a075d
Tyler <tysawyers@gmail.com>
2019-03-20 15:30:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//console.log("test")
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
+
+var arr = [1, 1, 2, 2, 3, 5]
+ 
+require("uniq")(arr)
+console.log(chalk.blue(require("uniq")(arr)))
